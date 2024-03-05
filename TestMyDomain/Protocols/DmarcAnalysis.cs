de5c9c1e438fced7a4f777bd8b593d1b3bf8c0c3
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-03-05 19:35:33 +0100

    Improve dmarc record analysis

--- TestMyDomain/Protocols/DmarcAnalysis.cs
+++ TestMyDomain/Protocols/DmarcAnalysis.cs
@@ -21,17 +21,30 @@ namespace TestMyDomain.Protocols {
         public bool DmarcRecordExists { get; private set; } // should be true
         public bool StartsCorrectly { get; private set; } // should be true
         public bool ExceedsCharacterLimit { get; private set; } // should be false
-        public string Policy { get; private set; }
+
+        public string Policy => TranslatePolicy(PolicyShort);
+        public string SubPolicy => TranslatePolicy(SubPolicyShort);
+        public string ReportingInterval => TranslateReportingInterval(ReportingIntervalShort);
+        public string Percent => TranslatePercentage(Pct.ToString());
+        public string SpfAlignment => TranslateAlignment(SpfAShort);
+        public string DkimAlignment => TranslateAlignment(DkimAShort);
+        public string FailureReportingOptions => TranslateFailureReportingOptions(FoShort);
+
         public string Rua { get; private set; }
+        public List<string> MailtoRua { get; private set; } = new List<string>();
+        public List<string> HttpRua { get; private set; } = new List<string>();
         public string Ruf { get; private set; }
-        public int Pct { get; private set; }
+        public List<string> MailtoRuf { get; private set; } = new List<string>();
+        public List<string> HttpRuf { get; private set; } = new List<string>();
 
-
-        public string SubPolicy { get; private set; }
-        public int ReportingInterval { get; private set; }
-        public string FailureReportingOptions { get; private set; }
-        public string DkimAlignment { get; private set; }
-        public string SpfAlignment { get; private set; }
+        // short versions of the tags
+        public string SubPolicyShort { get; private set; }
+        public string PolicyShort { get; private set; }
+        public string FoShort { get; private set; }
+        public string DkimAShort { get; private set; }
+        public string SpfAShort { get; private set; }
+        public int? Pct { get; private set; }
+        public int ReportingIntervalShort { get; private set; }
 
         public async Task AnalyzeDmarcRecords(IEnumerable<DnsResult> dnsResults, InternalLogger logger) {
             var dmarcRecordList = dnsResults.ToList();
@@ -42,6 +55,11 @@ namespace TestMyDomain.Protocols {
                 foreach (var data in record.Data) {
                     DmarcRecord = data;
                 }
+            }
+
+            if (DmarcRecord == null) {
+                logger.WriteVerbose("No DMARC record found.");
+                return;
             }
 
             logger.WriteVerbose($"Analyzing DMARC record {DmarcRecord}");
@@ -61,32 +79,102 @@ namespace TestMyDomain.Protocols {
                     var value = keyValue[1].Trim();
                     switch (key) {
                         case "p":
-                            Policy = value;
+                            PolicyShort = value;
                             break;
                         case "sp":
-                            SubPolicy = value;
+                            SubPolicyShort = value;
                             break;
                         case "ri":
-                            ReportingInterval = int.Parse(value);
+                            ReportingIntervalShort = int.Parse(value);
                             break;
                         case "fo":
-                            FailureReportingOptions = value;
+                            FoShort = value;
+                            break;
+                        case "pct":
+                            Pct = int.Parse(value);
                             break;
                         case "adkim":
-                            DkimAlignment = value;
+                            DkimAShort = value;
                             break;
                         case "aspf":
-                            SpfAlignment = value;
+                            SpfAShort = value;
                             break;
                         case "rua":
                             Rua = value;
+                            AddUriToList(value, MailtoRua, HttpRua);
                             break;
                         case "ruf":
                             Ruf = value;
+                            AddUriToList(value, MailtoRuf, HttpRuf);
                             break;
                     }
                 }
             }
+            // set the default value for the pct tag if it is not present
+            Pct ??= 100;
         }
+
+        private void AddUriToList(string uri, List<string> mailtoList, List<string> httpList) {
+            var uris = uri.Split(',');
+            foreach (var u in uris) {
+                if (u.StartsWith("mailto:")) {
+                    mailtoList.Add(u.Substring(7));
+                } else if (u.StartsWith("http:")) {
+                    httpList.Add(u);
+                }
+            }
+        }
+        private string TranslateAlignment(string alignment) {
+            switch (alignment) {
+                case "s":
+                    return "strict";
+                case "r":
+                    return "relaxed";
+                default:
+                    return "relaxed (default)"; // default to relaxed if no value is provided
+            }
+        }
+
+        private string TranslatePolicy(string policy) {
+            switch (policy) {
+                case "none":
+                    return "No policy";
+                case "quarantine":
+                    return "Quarantine";
+                case "reject":
+                    return "Reject";
+                default:
+                    return "Unknown policy";
+            }
+        }
+
+        private string TranslateFailureReportingOptions(string option) {
+            switch (option) {
+                case "0":
+                    return "Generate a DMARC failure report if all underlying authentication mechanisms fail to produce an aligned 'pass' result.";
+                case "1":
+                    return "Generate a DMARC failure report if any underlying authentication mechanism produced something other than an aligned 'pass' result.";
+                case "d":
+                    return "Generate a DKIM failure report if the message had a signature that failed evaluation.";
+                case "s":
+                    return "Generate an SPF failure report if the message failed SPF evaluation.";
+                default:
+                    return "Unknown option";
+            }
+        }
+
+        private string TranslatePercentage(string pct) {
+            int pctValue;
+            if (int.TryParse(pct, out pctValue)) {
+                return $"{pctValue}% of messages are subjected to filtering.";
+            } else {
+                return "Invalid percentage value.";
+            }
+        }
+
+        private string TranslateReportingInterval(int interval) {
+            return $"{interval / 86400} days";
+        }
+
     }
 }
