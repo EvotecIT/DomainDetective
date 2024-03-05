ef6094e665f79edb2f49ec4ba0865bb1b90e4934
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-03-05 11:09:03 +0100

    Add example project

---
+++ TestMyDomain.Example/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Threading.Tasks;
+using TestMyDomain;
+
+public static partial class Program {
+    public static async Task Main() {
+        // run the analysis of SPF
+        await ExampleAnalyseSPF();
+    }
+
+    public static void ShowProperties(string analysisOf, object obj) {
+        Console.WriteLine("----");
+        Console.WriteLine($"Analysis of {analysisOf}:");
+        var properties = obj.GetType().GetProperties();
+        foreach (var property in properties) {
+            var value = property.GetValue(obj);
+            if (value is IList listValue) {
+                Console.WriteLine($"- {property.Name}:");
+                foreach (var item in listValue) {
+                    Console.WriteLine($"  * {item}");
+                }
+            } else {
+                Console.WriteLine($"- {property.Name}: {value}");
+            }
+        }
+    }
+}
