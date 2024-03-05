a69705999c3505ef9eb748f5591cb90d39583103
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-03-05 11:41:32 +0100

    Improve tests for SPF

--- TestMyDomain.Tests/TestSpfAnalysis.cs
+++ TestMyDomain.Tests/TestSpfAnalysis.cs
@@ -2,19 +2,19 @@ namespace TestMyDomain.Tests {
     public class TestSpfAnalysis {
         [Fact]
         public async Task TestSpfNullsAndExceedDnsLookups() {
-            var spfRecord3 = "v=spf1 ip4: include: test.example.pl include:_spf-a.example.com include:_spf-b.example.com include:_spf-c.example.com include:_spf-ssg-a.example.com include:spf-a.anotherexample.com ip4:131.107.115.215 ip4:131.107.115.214 ip4:205.248.106.64 ip4:205.248.106.30 ip4:205.248.106.32 ~all";
+            var spfRecord3 = "v=spf1 ip4: include: test.example.pl a:google.com a:test.com ip4: include: test.example.pl include:_spf.salesforce.com include:_spf.google.com include:spf.protection.outlook.com include:_spf-a.example.com include:_spf-b.example.com include:_spf-c.example.com include:_spf-ssg-a.example.com include:spf-a.anotherexample.com ip4:131.107.115.215 ip4:131.107.115.214 ip4:205.248.106.64 ip4:205.248.106.30 ip4:205.248.106.32 ~all";
             var healthCheck6 = new DomainHealthCheck();
             await healthCheck6.CheckSPF(spfRecord3);
 
             Assert.True(healthCheck6.SpfAnalysis.SpfRecordExists);
             Assert.False(healthCheck6.SpfAnalysis.MultipleSpfRecords);
             Assert.True(healthCheck6.SpfAnalysis.HasNullLookups == true);
-            //Assert.True(healthCheck6.SpfAnalysis.ExceedsDnsLookups == false);
+            Assert.True(healthCheck6.SpfAnalysis.ExceedsDnsLookups == true, "Exceeds lookups should be true, as we expect it over the board");
+            Assert.True(healthCheck6.SpfAnalysis.DnsLookupsCount == 13, "DNS lookups should be 15, as we did it on purpose and got: " + healthCheck6.SpfAnalysis.DnsLookupsCount);
             Assert.True(healthCheck6.SpfAnalysis.MultipleAllMechanisms == false);
             Assert.True(healthCheck6.SpfAnalysis.ContainsCharactersAfterAll == false);
             Assert.True(healthCheck6.SpfAnalysis.HasPtrType == false);
             Assert.True(healthCheck6.SpfAnalysis.StartsCorrectly == true);
-
         }
 
         [Fact]
@@ -26,8 +26,8 @@ namespace TestMyDomain.Tests {
 
             Assert.True(healthCheck6.SpfAnalysis.SpfRecordExists);
             Assert.False(healthCheck6.SpfAnalysis.MultipleSpfRecords);
-            Assert.True(healthCheck6.SpfAnalysis.HasNullLookups == true);
-            //Assert.True(healthCheck6.SpfAnalysis.ExceedsDnsLookups == false);
+            Assert.True(healthCheck6.SpfAnalysis.HasNullLookups == false);
+            Assert.True(healthCheck6.SpfAnalysis.ExceedsDnsLookups == false);
             Assert.True(healthCheck6.SpfAnalysis.MultipleAllMechanisms == false);
             Assert.True(healthCheck6.SpfAnalysis.ContainsCharactersAfterAll == false);
             Assert.True(healthCheck6.SpfAnalysis.HasPtrType == false);
