a6d2c618bf86a4f0192a934c1c7fdf7b12f072e7
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-03-15 20:29:02 +0100

    Improve testing

--- DomainDetective.Tests/TestALL.cs
+++ DomainDetective.Tests/TestALL.cs
@@ -2,9 +2,10 @@
     public class TestAll {
         [Fact]
         public async void TestAllHealthChecks() {
-            var healthCheck = new DomainHealthCheck();
-            healthCheck.Verbose = true;
-            await healthCheck.Verify("evotec.pl", [HealthCheckType.DMARC, HealthCheckType.SPF, HealthCheckType.DKIM], ["selector1", "selector2"]);
+            var healthCheck = new DomainHealthCheck {
+                Verbose = false
+            };
+            await healthCheck.Verify("evotec.pl", [HealthCheckType.DMARC, HealthCheckType.SPF, HealthCheckType.DKIM, HealthCheckType.CAA], ["selector1", "selector2"]);
 
             Assert.True(healthCheck.DmarcAnalysis.Pct == 100);
             Assert.True(healthCheck.DmarcAnalysis.PolicyShort == "reject");
@@ -39,6 +40,15 @@
             Assert.True(healthCheck.SpfAnalysis.HasPtrType == false);
             Assert.True(healthCheck.SpfAnalysis.StartsCorrectly == true);
             Assert.True(healthCheck.SpfAnalysis.ExceedsCharacterLimit == false);
+
+            Assert.True(healthCheck.CAAAnalysis.AnalysisResults.Count == 8);
+            Assert.True(healthCheck.CAAAnalysis.Valid == true);
+            Assert.True(healthCheck.CAAAnalysis.Conflicting == false);
+            Assert.True(healthCheck.CAAAnalysis.ConflictingWildcardCertificateIssuance == false);
+            Assert.True(healthCheck.CAAAnalysis.ConflictingCertificateIssuance == false);
+            Assert.True(healthCheck.CAAAnalysis.CanIssueMail.Count == 0);
+            Assert.True(healthCheck.CAAAnalysis.CanIssueWildcardCertificatesForDomain.Count == 4);
+            Assert.True(healthCheck.CAAAnalysis.CanIssueCertificatesForDomain.Count == 4);
         }
     }
 }
