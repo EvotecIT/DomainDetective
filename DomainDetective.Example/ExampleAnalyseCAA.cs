f9fc4ed097ecb161bca877156342ab2d8fe5239a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-03-15 18:31:06 +0100

    Add more examples for testing

--- DomainDetective.Example/ExampleAnalyseCAA.cs
+++ DomainDetective.Example/ExampleAnalyseCAA.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using DnsClient.Protocol;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace DomainDetective.Example;
 
@@ -11,6 +13,27 @@ public static partial class Program {
         //foreach (var analysis in healthCheck.CAAAnalysis.AnalysisResults) {
         //    ShowProperties("CAA Example Record", analysis);
         //}
+        Helpers.ShowPropertiesTable(analysisOf: "CAA Example by String", objs: healthCheck.CAAAnalysis);
+        Helpers.ShowPropertiesTable(analysisOf: "CAA Example by String", objs: healthCheck.CAAAnalysis.AnalysisResults);
+    }
+
+    public static async Task ExampleAnalysByListCAA() {
+        List<string> caaRecords = new List<string> {
+            "0 issue \"digicert.com; cansignhttpexchanges=yes\"",
+            "0 issue \"letsencrypt.org;validationmethods=dns-01\"",
+            "0 issue \"pki.goog; cansignhttpexchanges=yes\"",
+            "0 issuewild \"letsencrypt.org\"",
+            "0 issue \"letsencrypt.org\"",
+            "0 iodef \"mailto:example@example.com\"",
+            "260 issue \";\"",
+            "0 issue \"letsencrypt.org\"",
+            "0 issuemail \";\""
+        };
+        var healthCheck = new DomainHealthCheck();
+        healthCheck.Verbose = false;
+        await healthCheck.CheckCAA(caaRecords);
+        Helpers.ShowPropertiesTable(analysisOf: "CAA Example by String", objs: healthCheck.CAAAnalysis);
+
         Helpers.ShowPropertiesTable(analysisOf: "CAA Example by String", objs: healthCheck.CAAAnalysis.AnalysisResults);
     }
 
@@ -21,6 +44,7 @@ public static partial class Program {
         //foreach (var analysis in healthCheck.CAAAnalysis.AnalysisResults) {
         //    ShowProperties("CAA Example Record", analysis);
         //}
+        Helpers.ShowPropertiesTable(analysisOf: "CAA Example Record", objs: healthCheck.CAAAnalysis);
         Helpers.ShowPropertiesTable(analysisOf: "CAA Example Record", objs: healthCheck.CAAAnalysis.AnalysisResults);
     }
 }
