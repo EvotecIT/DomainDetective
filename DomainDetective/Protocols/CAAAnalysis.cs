4397a69aa3163619eb6c0757ebc04d0318d5c0b3
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-03-15 18:30:39 +0100

    Proper CAA analysis

--- DomainDetective/Protocols/CAAAnalysis.cs
+++ DomainDetective/Protocols/CAAAnalysis.cs
@@ -1,50 +1,209 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
-namespace DomainDetective;
+namespace DomainDetective {
 
-public class CAAAnalysis {
-    public List<CAARecordAnalysis> AnalysisResults { get; private set; } = new List<CAARecordAnalysis>();
+    public class CAAAnalysis {
+        public string DomainName { get; set; }
 
-    public async Task AnalyzeCAARecords(IEnumerable<DnsResult> dnsResults, InternalLogger logger) {
-        var caaRecordList = dnsResults.ToList();
+        public string Description { get; set; } =
+            @"A Certification Authority Authorization (CAA) record allows a domain to specify which certificate authorities (CAs)
+are permitted to issue certificates for it.
 
-        // create a single string from the list of DnsResult objects
-        foreach (var record in caaRecordList) {
-            var analysis = new CAARecordAnalysis();
+By using CAA records, domain owners have the ability to state which certificate authorities can issue a certificate for their domain.
+These records also offer a way to set notification rules in case a certificate is requested from a certificate authority that is not authorized.
 
-            foreach (var data in record.Data) {
-                var caaRecord = data;
+In the absence of a CAA record, any CA has the permission to issue a certificate for the domain.
+However, if a CAA record exists, only the CAs listed in that record(s) have the authorization to issue certificates for the hostname.
 
-                logger.WriteVerbose($"Analyzing CAA record {caaRecord}");
+CAA records can establish policy for the entire domain or for specific hostnames, and these policies are inherited by subdomains.
+As an illustration, a CAA record that is set on example.com is also applicable to subdomain.example.com.";
+        public int ValidRecords { get; private set; }
+        public int InvalidRecords { get; private set; }
+        public List<string> CanIssueCertificatesForDomain { get; set; } = new List<string>();
+        public List<string> CanIssueWildcardCertificatesForDomain { get; set; } = new List<string>();
 
-                // loop through the properties of the CAA record
-                var properties = caaRecord.Split(' ');
-                if (properties.Length == 3) {
-                    var key = properties[1].Trim();
-                    var value = properties[2].Trim().Trim('"'); // remove quotes from value
-                    switch (key) {
-                        case "issue":
-                            analysis.Issue.Add(value);
-                            break;
-                        case "issuewild":
-                            analysis.Issuewild.Add(value);
-                            break;
-                        case "iodef":
-                            analysis.Iodef.Add(value);
-                            break;
+        public List<CAARecordAnalysis> AnalysisResults { get; private set; } = new List<CAARecordAnalysis>();
+
+        public async Task AnalyzeCAARecords(IEnumerable<DnsResult> dnsResults, InternalLogger logger) {
+            var caaRecordList = dnsResults.ToList();
+
+            DomainName = caaRecordList.First().Name;
+
+            foreach (var record in caaRecordList) {
+                var analysis = new CAARecordAnalysis();
+
+                foreach (var data in record.Data) {
+                    var caaRecord = data;
+
+                    logger.WriteVerbose($"Analyzing CAA record {caaRecord}");
+
+                    analysis.CAARecord = caaRecord;
+
+                    var properties = caaRecord.Split(new[] { ' ' }, 3); // Split into 3 parts at most
+                    if (properties.Length == 3) {
+                        var flag = int.Parse(properties[0].Trim());
+                        var tag = properties[1].Trim();
+                        var value = properties[2];
+
+                        // Validate flag
+                        analysis.Flag = flag.ToString();
+                        if (flag < 0 || flag > 255) {
+                            analysis.InvalidFlag = true;
+                        }
+
+                        // Validate tag and set the Tag property
+                        var validTags = new Dictionary<string, CAATagType> {
+                            { "issue", CAATagType.Issue },
+                            { "issuewild", CAATagType.Issuewild },
+                            { "iodef", CAATagType.Iodef },
+                            { "issuemail", CAATagType.Issuemail }
+                        };
+                        if (validTags.TryGetValue(tag, out var tagType)) {
+                            analysis.Tag = tagType;
+                        } else {
+                            analysis.Tag = CAATagType.Unknown;
+                            analysis.InvalidTag = true;
+                            //continue;
+                        }
+
+                        // Validate value
+                        // Validate value
+                        bool isValueQuoted = value.Length >= 2 && value[0] == '"' && value[value.Length - 1] == '"';
+                        if (isValueQuoted || !value.Contains(" ")) {
+                            if (isValueQuoted) {
+                                // Remove the wrapping double quotes
+                                value = value.Substring(1, value.Length - 2);
+
+                                // Check for unescaped inner double quotes
+                                if (value.Contains("\"")) {
+                                    analysis.InvalidValueUnescapedQuotes = true;
+                                    //  continue;
+                                }
+
+                                // Replace escaped double quotes with actual double quotes
+                                value = value.Replace("\\\"", "\"");
+                            }
+
+                            // Existing code for additional validation...
+                            analysis.Value = value; // Move this line outside the if block
+                        } else {
+                            analysis.InvalidValueUnescapedQuotes = true;
+                            //continue;
+                        }
+
+                        // Additional validation for issue, issuewild, and issuemail tags
+                        if (tagType == CAATagType.Issue || tagType == CAATagType.Issuewild || tagType == CAATagType.Issuemail) {
+                            var isValueOnlySemicolon = value == ";";
+                            if (isValueOnlySemicolon) {
+                                analysis.Value = value;
+                                continue;
+                            }
+                            var parts = value.Split(new[] { ';' }, 2); // Split into 2 parts at most
+                            var domainName = parts[0].Trim();
+                            if (string.IsNullOrEmpty(domainName)) {
+                                // The domain name can be left empty, which must be indicated providing just ";" as a value
+                                if (parts.Length > 1) {
+                                    analysis.InvalidValueWrongParameters = true;
+                                    // continue;
+                                }
+
+                            } else {
+                                // It must contain a domain name
+                                if (!Uri.TryCreate($"http://{domainName}", UriKind.Absolute, out _)) {
+                                    analysis.InvalidValueWrongDomain = true;
+                                    // continue;
+                                }
+                            }
+
+                            analysis.CAName = domainName;
+
+                            // Parse additional parameters
+                            var parameters = new Dictionary<string, string>();
+                            if (parts.Length > 1) {
+                                var paramParts = parts[1].Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                                for (int i = 0; i < paramParts.Length; i++) {
+                                    var trimmedPart = paramParts[i].Trim();
+                                    var keyValue = trimmedPart.Split('=');
+                                    if (keyValue.Length == 2) {
+                                        parameters[keyValue[0].Trim()] = keyValue[1].Trim(); // Trim the keys and values
+                                    } else {
+                                        analysis.InvalidValueWrongParameters = true;
+                                        // continue;
+                                    }
+                                }
+                            }
+
+
+
+                            analysis.Parameters = parameters;
+                        }
+                        analysis.Value = value;
+                    } else {
+                        analysis.InvalidFlag = true;
+                        analysis.InvalidTag = true;
+                        analysis.InvalidValueUnescapedQuotes = true;
                     }
                 }
+
+                if (analysis.InvalidFlag || analysis.InvalidTag || analysis.InvalidValueUnescapedQuotes || analysis.InvalidValueWrongDomain || analysis.InvalidValueWrongParameters) {
+                    InvalidRecords++;
+                    analysis.Invalid = true;
+                } else {
+                    ValidRecords++;
+                    analysis.Invalid = false;
+                }
+
+                AnalysisResults.Add(analysis);
             }
 
-            AnalysisResults.Add(analysis);
+            GenerateSummary();
+        }
+        public void GenerateSummary() {
+            // var summary = new StringBuilder();
+            // summary.AppendLine($"Interpreted CAA records\nHere's what the found CAA records mean.\n");
+            // summary.AppendLine($"The following Certificate Authorities can issue certificates for the domain evotec.pl.\n");
+
+            foreach (var analysis in AnalysisResults.Where(a => !a.InvalidFlag && !a.InvalidTag && !a.InvalidValueUnescapedQuotes && !a.InvalidValueWrongDomain && !a.InvalidValueWrongParameters)) {
+                //   summary.AppendLine(analysis.Value);
+                CanIssueCertificatesForDomain.Add(analysis.CAName);
+            }
+
+            // summary.AppendLine($"The following Certificate Authorities can issue wildcard certificates (*.evotec.pl).\n");
+
+            foreach (var analysis in AnalysisResults.Where(a => !a.InvalidFlag && !a.InvalidTag && !a.InvalidValueUnescapedQuotes && !a.InvalidValueWrongDomain && !a.InvalidValueWrongParameters && a.Tag == CAATagType.Issuewild)) {
+                //   summary.AppendLine(analysis.Value);
+                CanIssueWildcardCertificatesForDomain.Add(analysis.CAName);
+            }
+
+            // return summary.ToString();
         }
     }
-}
 
-public class CAARecordAnalysis {
-    public List<string> Issue { get; set; } = new List<string>();
-    public List<string> Issuewild { get; set; } = new List<string>();
-    public List<string> Iodef { get; set; } = new List<string>();
+    public class CAARecordAnalysis {
+        public string CAARecord { get; set; }
+        public string Flag { get; set; }
+        public CAATagType Tag { get; set; }
+        public string Value { get; set; }
+        public string CAName { get; set; }
+
+        public bool Invalid { get; set; }
+        public bool InvalidFlag { get; set; }
+        public bool InvalidTag { get; set; }
+        public bool InvalidValueUnescapedQuotes { get; set; }
+        public bool InvalidValueWrongDomain { get; set; }
+        public bool InvalidValueWrongParameters { get; set; }
+        public Dictionary<string, string> Parameters { get; set; }
+    }
+
+    public enum CAATagType {
+        Unknown,
+        Issue,
+        Issuewild,
+        Iodef,
+        Issuemail
+    }
 }
