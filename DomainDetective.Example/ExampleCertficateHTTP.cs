4e1e8627ce448087a8f783370c00aeba244197e7
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-05 20:03:33 +0200

    example updates

---
+++ DomainDetective.Example/ExampleCertficateHTTP.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainDetective;
+
+namespace DomainDetective.Example;
+
+public static partial class Program {
+
+    public static async Task ExampleCertificateVerification() {
+        var analysis = await CertificateAnalysis.CheckWebsiteCertificate("https://google.com");
+        Helpers.ShowPropertiesTable("Certificate for google.com", analysis);
+        Helpers.ShowPropertiesTable("Certificate for google.com", analysis.Certificate);
+    }
+
+    public static async Task ExampleCertificateVerificationByHealthCheck() {
+        var healthCheck = new DomainHealthCheck();
+        healthCheck.Verbose = false;
+        await healthCheck.VerifyWebsiteCertificate("https://evotec.pl");
+        Helpers.ShowPropertiesTable("Certificate for evotec.pl ", healthCheck.CertificateAnalysis);
+        Helpers.ShowPropertiesTable("Certificate for evotec.pl ", healthCheck.CertificateAnalysis.Certificate);
+    }
+}
--- DomainDetective.Example/Program.cs
+++ DomainDetective.Example/Program.cs
@@ -4,23 +4,32 @@ namespace DomainDetective.Example;
 
 public static partial class Program {
     public static async Task Main() {
-        // run the analysis of SPF
         //await ExampleAnalyseByDnsSPF();
         //await ExampleAnalyseByStringSPF();
-        // run the analysis of DMARC
+
         //await ExampleAnalyseByStringDMARC();
         //await ExampleAnalyseByDomainDMARC();
-        // run the analysis of DKIM
+
         //await ExampleAnalyseByStringDKIM();
         //await ExampleAnalyseByDomainDKIM();
-        // run the analysis of MX
+
         //await ExampleAnalyseMX();
-        // run the analysis of CAA
+
         //await ExampleAnalyseByDomainCAA();
         //await ExampleAnalyseByStringCAA();
-        //await ExampleAnalysByListCAA();
+        //await ExampleAnalyseByListCAA();
 
-        await ExampleAnalyseByDomainDANE();
-        await ExampleAnalyseByStringDANE();
+        //await ExampleCertificateVerification();
+        //await ExampleCertificateVerificationByHealthCheck();
+
+
+        //await ExampleAnalyseByDomainDANE();
+        //await ExampleAnalyseByStringDANE();
+
+        await ExampleAnalyseByStringDNSBL();
+        // await ExampleAnalyseByDomainDNSBL();
+
+
+        //await ExampleQueryDNS();
     }
 }
