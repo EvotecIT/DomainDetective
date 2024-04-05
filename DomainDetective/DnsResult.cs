127c1df27ee23cdd6ec6087e1b85f2749ebb14fc
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-05 19:43:43 +0200

    Refactor DNSResult class in DomainDetective namespace

--- DomainDetective/DnsResult.cs
+++ DomainDetective/DnsResult.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using DnsClient;
-using DnsClient.Protocol;
 using DnsClientX;
 
 namespace DomainDetective {
@@ -12,45 +10,5 @@ namespace DomainDetective {
         public string DataJoined { get; set; }
 
         internal ServiceType ServiceType { get; set; }
-
-        public static IEnumerable<DnsResult> TranslateFromDnsQueryResponse(IDnsQueryResponse response, string type, string filter) {
-            if (response == null || response.Answers == null) {
-                yield break;
-            }
-            if (type == "TXT") {
-                foreach (TxtRecord answer in response.Answers) {
-                    // we join text to be able to filter properly on the whole string
-                    var data = string.Join("", answer.EscapedText);
-                    if (filter != null && !data.ToLowerInvariant().Contains(filter.ToLowerInvariant())) {
-                        continue;
-                    }
-                    var dnsResult = new DnsResult {
-                        Name = answer.DomainName,
-                        Data = answer.EscapedText.ToArray(),
-                        DataJoined = data,
-                    };
-                    yield return dnsResult;
-                }
-            }
-        }
-
-        public static IEnumerable<DnsResult> TranslateFromDohResponse(DnsResponse response, string type, string filter) {
-            if (response.Answers == null) {
-                yield break;
-            }
-            foreach (var answer in response.Answers) {
-                // we join text to be able to filter properly on the whole string
-                var data = answer.Data;
-                if (filter != null && !data.ToLowerInvariant().Contains(filter.ToLowerInvariant())) {
-                    continue;
-                }
-                var dnsResult = new DnsResult {
-                    Name = answer.Name,
-                    Data = answer.DataStringsEscaped,
-                    DataJoined = answer.Data
-                };
-                yield return dnsResult;
-            }
-        }
     }
 }
