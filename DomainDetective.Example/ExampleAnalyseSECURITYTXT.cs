7f644331dedb6bf682ca95e2046a1a2781078ead
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-06 15:00:18 +0200

    Add example

---
+++ DomainDetective.Example/ExampleAnalyseSECURITYTXT.cs
@@ -0,0 +1,27 @@
+using System.Threading.Tasks;
+
+namespace DomainDetective.Example;
+
+public static partial class Program {
+    public static async Task ExampleAnalyseSecurityTXT() {
+        var healthCheck = new DomainHealthCheck();
+        healthCheck.Verbose = false;
+        await healthCheck.Verify("google.com", new[] { HealthCheckType.SECURITYTXT });
+        Helpers.ShowPropertiesTable(analysisOf: "SECURITYTXT for google.com", objs: healthCheck.SecurityTXTAnalysis);
+
+        await healthCheck.Verify("www.gemini.com", new[] { HealthCheckType.SECURITYTXT });
+        Helpers.ShowPropertiesTable(analysisOf: "SECURITYTXT for gemini.com", objs: healthCheck.SecurityTXTAnalysis);
+
+        await healthCheck.Verify("www.facebook.com", new[] { HealthCheckType.SECURITYTXT });
+        Helpers.ShowPropertiesTable(analysisOf: "SECURITYTXT for www.facebook.com", objs: healthCheck.SecurityTXTAnalysis);
+
+        await healthCheck.Verify("evotec.xyz", new[] { HealthCheckType.SECURITYTXT });
+        Helpers.ShowPropertiesTable(analysisOf: "SECURITYTXT for evotec.xyz", objs: healthCheck.SecurityTXTAnalysis);
+
+        await healthCheck.Verify("gov.uk", new[] { HealthCheckType.SECURITYTXT });
+        Helpers.ShowPropertiesTable(analysisOf: "SECURITYTXT for gov.uk", objs: healthCheck.SecurityTXTAnalysis);
+
+        await healthCheck.Verify("securitytxt.org", new[] { HealthCheckType.SECURITYTXT });
+        Helpers.ShowPropertiesTable(analysisOf: "SECURITYTXT for securitytxt.org", objs: healthCheck.SecurityTXTAnalysis);
+    }
+}
