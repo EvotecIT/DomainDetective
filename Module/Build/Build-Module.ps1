0f3320f98b591def87460e2336efa1c03fb7786e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-23 09:00:41 +0200

    chore: Add new module files, update gitignore, and refactor classes in DomainDetective namespace

---
+++ Module/Build/Build-Module.ps1
@@ -0,0 +1,90 @@
+﻿Import-Moddule PSPublishModule -Force
+
+Build-Module -ModuleName 'DomainDetective' {
+    # Usual defaults as per standard module
+    $Manifest = [ordered] @{
+        ModuleVersion        = '0.1.0'
+        CompatiblePSEditions = @('Desktop', 'Core')
+        GUID                 = 'a2986f0d-da11-43f5-a252-f9e1d1699776'
+        Author               = 'Przemyslaw Klys'
+        CompanyName          = 'Evotec'
+        Copyright            = "(c) 2011 - $((Get-Date).Year) Przemyslaw Klys @ Evotec. All rights reserved."
+        Description          = 'DomainDetective is a PowerShell module that provides features to work with domains, DNS, and other related information.'
+        Tags                 = @('Windows', 'MacOS', 'Linux')
+        #IconUri              = ''
+        ProjectUri           = 'https://github.com/EvotecIT/DomainDetective'
+        PowerShellVersion    = '5.1'
+    }
+    New-ConfigurationManifest @Manifest
+
+
+    $ConfigurationFormat = [ordered] @{
+        RemoveComments                              = $false
+
+        PlaceOpenBraceEnable                        = $true
+        PlaceOpenBraceOnSameLine                    = $true
+        PlaceOpenBraceNewLineAfter                  = $true
+        PlaceOpenBraceIgnoreOneLineBlock            = $false
+
+        PlaceCloseBraceEnable                       = $true
+        PlaceCloseBraceNewLineAfter                 = $false
+        PlaceCloseBraceIgnoreOneLineBlock           = $false
+        PlaceCloseBraceNoEmptyLineBefore            = $true
+
+        UseConsistentIndentationEnable              = $true
+        UseConsistentIndentationKind                = 'space'
+        UseConsistentIndentationPipelineIndentation = 'IncreaseIndentationAfterEveryPipeline'
+        UseConsistentIndentationIndentationSize     = 4
+
+        UseConsistentWhitespaceEnable               = $true
+        UseConsistentWhitespaceCheckInnerBrace      = $true
+        UseConsistentWhitespaceCheckOpenBrace       = $true
+        UseConsistentWhitespaceCheckOpenParen       = $true
+        UseConsistentWhitespaceCheckOperator        = $true
+        UseConsistentWhitespaceCheckPipe            = $true
+        UseConsistentWhitespaceCheckSeparator       = $true
+
+        AlignAssignmentStatementEnable              = $true
+        AlignAssignmentStatementCheckHashtable      = $true
+
+        UseCorrectCasingEnable                      = $true
+    }
+    # format PSD1 and PSM1 files when merging into a single file
+    # enable formatting is not required as Configuration is provided
+    New-ConfigurationFormat -ApplyTo 'OnMergePSM1', 'OnMergePSD1' -Sort None @ConfigurationFormat
+    # format PSD1 and PSM1 files within the module
+    # enable formatting is required to make sure that formatting is applied (with default settings)
+    New-ConfigurationFormat -ApplyTo 'DefaultPSD1', 'DefaultPSM1' -EnableFormatting -Sort None
+    # when creating PSD1 use special style without comments and with only required parameters
+    New-ConfigurationFormat -ApplyTo 'DefaultPSD1', 'OnMergePSD1' -PSD1Style 'Minimal'
+
+    # configuration for documentation, at the same time it enables documentation processing
+    New-ConfigurationDocumentation -Enable:$false -StartClean -UpdateWhenNew -PathReadme 'Docs\Readme.md' -Path 'Docs'
+
+    New-ConfigurationImportModule -ImportSelf -ImportRequiredModules
+
+    $newConfigurationBuildSplat = @{
+        Enable                            = $true
+        SignModule                        = $true
+        MergeModuleOnBuild                = $true
+        MergeFunctionsFromApprovedModules = $true
+        CertificateThumbprint             = '483292C9E317AA13B07BB7A96AE9D1A5ED9E7703'
+        NETProjectPath                    = "$PSScriptRoot\..\..\DomainDetective.PowerShell"
+        ResolveBinaryConflicts            = $false
+        ResolveBinaryConflictsName        = 'DomainDetective.PowerShell'
+        NETProjectName                    = 'DomainDetective.PowerShell'
+        NETBinaryModule                   = 'DomainDetective.PowerShell.dll'
+        NETConfiguration                  = 'Release'
+        NETFramework                      = 'net472', 'net6.0'
+        DotSourceLibraries                = $true
+    }
+
+    New-ConfigurationBuild @newConfigurationBuildSplat
+
+    New-ConfigurationArtefact -Type Unpacked -Enable -Path "$PSScriptRoot\..\Artefacts\Unpacked" -RequiredModulesPath "$PSScriptRoot\..\Artefacts\Unpacked\Modules"
+    New-ConfigurationArtefact -Type Packed -Enable -Path "$PSScriptRoot\..\Artefacts\Packed" -IncludeTagName
+
+    # global options for publishing to github/psgallery
+    #New-ConfigurationPublish -Type PowerShellGallery -FilePath 'C:\Support\Important\PowerShellGalleryAPI.txt' -Enabled:$false
+    #New-ConfigurationPublish -Type GitHub -FilePath 'C:\Support\Important\GitHubAPI.txt' -UserName 'EvotecIT' -Enabled:$false
+}
---
+++ Module/Examples/Example.TestIpBlacklist.ps1
@@ -0,0 +1,13 @@
+﻿Clear-Host
+
+Import-Module $PSScriptRoot\..\DomainDetective.psd1 -Force
+
+Measure-Command {
+    $Blacklists = Test-DomainBlacklist -NameOrIpAddress 'google.com', "89.74.48.96" -Verbose -DnsEndpoint Cloudflare
+    $Blacklists | Sort-Object -Property IsBlackListed, Answer -Descending | Format-Table -AutoSize
+}
+
+Measure-Command {
+    $Blacklists1 = Test-DomainBlacklist -NameOrIpAddress "89.74.48.96" -Verbose
+    $Blacklists1 | Sort-Object -Property IsBlackListed, Answer -Descending | Format-Table -AutoSize
+}
