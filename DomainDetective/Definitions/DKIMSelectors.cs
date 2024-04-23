8605e3cef8edcad54aa0a2ec68ecf34faf4711de
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-23 08:59:00 +0200

    Refactor DKIMSelectors class in DomainDetective namespace

---
+++ DomainDetective/Definitions/DKIMSelectors.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainDetective.Definitions {
+    internal class DKIMSelectors {
+        //Google
+        // 
+        //     google
+        // 
+        // 
+        // Microsoft
+        // 
+        //     selector1
+        // 
+        //     selector2
+        // 
+        //     
+        // 
+        // Everlytic
+        // 
+        //     everlytickey1
+        // 
+        //     everlytickey2
+        // 
+        //     eversrv (OLD selector)
+        // 
+        //     
+        // 
+        // MailChimp / Mandrill
+        // 
+        //     k1
+        // 
+        //     
+        // 
+        // Global Micro 
+        // 
+        //     mxvault
+        // 
+        // 
+        // Hetzner
+        // 
+        //     dkim
+    }
+}
