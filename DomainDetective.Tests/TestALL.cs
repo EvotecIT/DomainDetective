5b1164be3ffd82058426f46154efdc1da5fc9f83
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-21 10:38:27 +0200

    fix: update assertions in TestALL.cs for consistency and clarity

--- DomainDetective.Tests/TestALL.cs
+++ DomainDetective.Tests/TestALL.cs
@@ -1,4 +1,4 @@
-﻿namespace DomainDetective.Tests {
+namespace DomainDetective.Tests {
     public class TestAll {
         [Fact]
         public async Task TestAllHealthChecks() {
@@ -7,48 +7,48 @@
             };
             await healthCheck.Verify("evotec.pl", [HealthCheckType.DMARC, HealthCheckType.SPF, HealthCheckType.DKIM, HealthCheckType.CAA], ["selector1", "selector2"]);
 
-            Assert.True(healthCheck.DmarcAnalysis.Pct == 100);
-            Assert.True(healthCheck.DmarcAnalysis.PolicyShort == "reject");
-            Assert.True(healthCheck.DmarcAnalysis.MailtoRua.Count == 3);
-            Assert.True(healthCheck.DmarcAnalysis.MailtoRua[0] == "1012c7e7df7b474cb85c1c8d00cc1c1a@dmarc-reports.cloudflare.net");
-            Assert.True(healthCheck.DmarcAnalysis.MailtoRua[1] == "7kkoc19n@ag.eu.dmarcian.com");
-            Assert.True(healthCheck.DmarcAnalysis.MailtoRua[2] == "dmarc@evotec.pl");
-            Assert.True(healthCheck.DmarcAnalysis.DkimAShort == "s");
-            Assert.True(healthCheck.DmarcAnalysis.SpfAShort == "s");
+            Assert.Equal(100, healthCheck.DmarcAnalysis.Pct);
+            Assert.Equal("reject", healthCheck.DmarcAnalysis.PolicyShort);
+            Assert.Equal(3, healthCheck.DmarcAnalysis.MailtoRua.Count);
+            Assert.Equal("1012c7e7df7b474cb85c1c8d00cc1c1a@dmarc-reports.cloudflare.net", healthCheck.DmarcAnalysis.MailtoRua[0]);
+            Assert.Equal("7kkoc19n@ag.eu.dmarcian.com", healthCheck.DmarcAnalysis.MailtoRua[1]);
+            Assert.Equal("dmarc@evotec.pl", healthCheck.DmarcAnalysis.MailtoRua[2]);
+            Assert.Equal("s", healthCheck.DmarcAnalysis.DkimAShort);
+            Assert.Equal("s", healthCheck.DmarcAnalysis.SpfAShort);
 
-            Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector1"].DkimRecordExists == true);
-            Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector1"].Flags == null);
-            Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector1"].HashAlgorithm == null);
-            Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector1"].KeyType == "rsa");
-            Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector1"].StartsCorrectly == true);
+            Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector1"].DkimRecordExists);
+            Assert.Null(healthCheck.DKIMAnalysis.AnalysisResults["selector1"].Flags);
+            Assert.Null(healthCheck.DKIMAnalysis.AnalysisResults["selector1"].HashAlgorithm);
+            Assert.Equal("rsa", healthCheck.DKIMAnalysis.AnalysisResults["selector1"].KeyType);
+            Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector1"].StartsCorrectly);
             Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector2"].KeyTypeExists);
 
-            Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector2"].DkimRecordExists == true);
-            Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector2"].Flags == null);
-            Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector2"].HashAlgorithm == null);
-            Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector2"].KeyType == "rsa");
+            Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector2"].DkimRecordExists);
+            Assert.Null(healthCheck.DKIMAnalysis.AnalysisResults["selector2"].Flags);
+            Assert.Null(healthCheck.DKIMAnalysis.AnalysisResults["selector2"].HashAlgorithm);
+            Assert.Equal("rsa", healthCheck.DKIMAnalysis.AnalysisResults["selector2"].KeyType);
             Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector2"].PublicKeyExists);
             Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector2"].StartsCorrectly);
             Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector2"].KeyTypeExists);
 
             Assert.True(healthCheck.SpfAnalysis.SpfRecordExists);
             Assert.False(healthCheck.SpfAnalysis.MultipleSpfRecords);
-            Assert.True(healthCheck.SpfAnalysis.HasNullLookups == false);
-            Assert.True(healthCheck.SpfAnalysis.ExceedsDnsLookups == false);
-            Assert.True(healthCheck.SpfAnalysis.MultipleAllMechanisms == false);
-            Assert.True(healthCheck.SpfAnalysis.ContainsCharactersAfterAll == false);
-            Assert.True(healthCheck.SpfAnalysis.HasPtrType == false);
-            Assert.True(healthCheck.SpfAnalysis.StartsCorrectly == true);
-            Assert.True(healthCheck.SpfAnalysis.ExceedsCharacterLimit == false);
+            Assert.False(healthCheck.SpfAnalysis.HasNullLookups);
+            Assert.False(healthCheck.SpfAnalysis.ExceedsDnsLookups);
+            Assert.False(healthCheck.SpfAnalysis.MultipleAllMechanisms);
+            Assert.False(healthCheck.SpfAnalysis.ContainsCharactersAfterAll);
+            Assert.False(healthCheck.SpfAnalysis.HasPtrType);
+            Assert.True(healthCheck.SpfAnalysis.StartsCorrectly);
+            Assert.False(healthCheck.SpfAnalysis.ExceedsCharacterLimit);
 
-            Assert.True(healthCheck.CAAAnalysis.AnalysisResults.Count == 8);
-            Assert.True(healthCheck.CAAAnalysis.Valid == true);
-            Assert.True(healthCheck.CAAAnalysis.Conflicting == false);
-            Assert.True(healthCheck.CAAAnalysis.ConflictingWildcardCertificateIssuance == false);
-            Assert.True(healthCheck.CAAAnalysis.ConflictingCertificateIssuance == false);
-            Assert.True(healthCheck.CAAAnalysis.CanIssueMail.Count == 0);
-            Assert.True(healthCheck.CAAAnalysis.CanIssueWildcardCertificatesForDomain.Count == 4);
-            Assert.True(healthCheck.CAAAnalysis.CanIssueCertificatesForDomain.Count == 4);
+            Assert.Equal(8, healthCheck.CAAAnalysis.AnalysisResults.Count);
+            Assert.True(healthCheck.CAAAnalysis.Valid);
+            Assert.False(healthCheck.CAAAnalysis.Conflicting);
+            Assert.False(healthCheck.CAAAnalysis.ConflictingWildcardCertificateIssuance);
+            Assert.False(healthCheck.CAAAnalysis.ConflictingCertificateIssuance);
+            Assert.Equal(0, healthCheck.CAAAnalysis.CanIssueMail.Count);
+            Assert.Equal(4, healthCheck.CAAAnalysis.CanIssueWildcardCertificatesForDomain.Count);
+            Assert.Equal(4, healthCheck.CAAAnalysis.CanIssueCertificatesForDomain.Count);
         }
     }
 }
