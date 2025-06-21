0c3304a0ee2e4b94a52db3b78693be88a5fb1922
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-21 11:09:08 +0200

    fix: add missing newlines at the end of multiple files for consistent formatting

--- DomainDetective.PowerShell/CmdletTestSpfRecord.cs
+++ DomainDetective.PowerShell/CmdletTestSpfRecord.cs
@@ -1,8 +1,7 @@
+using DnsClientX;
 using System.Linq;
 using System.Management.Automation;
 using System.Threading.Tasks;
-
-using DnsClientX;
 
 namespace DomainDetective.PowerShell {
     [Cmdlet(VerbsDiagnostic.Test, "SpfRecord", DefaultParameterSetName = "ServerName")]
