dd95a7e8e8a1e0fa442885b5c02e7a4e2119a185
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-21 20:20:11 +0200

    Remove CLI demo and tests

--- DomainDetective.CLI/Program.cs
+++ DomainDetective.CLI/Program.cs
@@ -1,31 +1,3 @@
-using DomainDetective;
 using System;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace DomainDetective.CLI {
-    public class Program {
-        public static Task<int> Main(string[] args) => Run(args);
-
-        public static async Task<int> Run(string[] args) {
-            if (args.Length != 2 || args[0].ToLowerInvariant() != "ns") {
-                Console.WriteLine("Usage: DomainDetective.CLI ns <domain>");
-                return 1;
-            }
-
-            var domain = args[1];
-            var healthCheck = new DomainHealthCheck();
-            await healthCheck.Verify(domain, new[] { HealthCheckType.NS });
-
-            foreach (var record in healthCheck.NSAnalysis.NsRecords) {
-                Console.WriteLine(record);
-            }
-
-            Console.WriteLine($"AtLeastTwoRecords: {healthCheck.NSAnalysis.AtLeastTwoRecords}");
-            Console.WriteLine($"HasDuplicates: {healthCheck.NSAnalysis.HasDuplicates}");
-            Console.WriteLine($"AllHaveAOrAaaa: {healthCheck.NSAnalysis.AllHaveAOrAaaa}");
-            Console.WriteLine($"PointsToCname: {healthCheck.NSAnalysis.PointsToCname}");
-            return 0;
-        }
-    }
-}
+Console.WriteLine("Hello, World!");
--- DomainDetective.Tests/TestCLI.cs
+++
@@ -1,9 +0,0 @@
-namespace DomainDetective.Tests {
-    public class TestCLI {
-        [Fact]
-        public async Task CliRunsNsCheck() {
-            var exitCode = await DomainDetective.CLI.Program.Run(new[] { "ns", "example.com" });
-            Assert.Equal(0, exitCode);
-        }
-    }
-}
