8785d318bd2e9d3f056aa356a9b05b6c6baf0f93
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-21 18:48:06 +0200

    Handle case-insensitive CAA tags and strict flag parsing

--- DomainDetective/Protocols/CAAAnalysis.cs
+++ DomainDetective/Protocols/CAAAnalysis.cs
@@ -1,6 +1,7 @@
 using DnsClientX;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -80,7 +81,7 @@ As an illustration, a CAA record that is set on example.com is also applicable t
                 // RFC 6844 section 5 specifies that the flag field must be a
                 // single unsigned octet in the range 0-255.  We validate the
                 // numeric value before processing the remaining parts.
-                if (properties.Length == 3 && int.TryParse(properties[0].Trim(), out var flag)) {
+                if (properties.Length == 3 && int.TryParse(properties[0].Trim(), NumberStyles.None, CultureInfo.InvariantCulture, out var flag)) {
                     var tag = properties[1].Trim();
                     var value = properties[2];
 
@@ -91,7 +92,7 @@ As an illustration, a CAA record that is set on example.com is also applicable t
                     }
 
                     // Validate tag and set the Tag property
-                    var validTags = new Dictionary<string, CAATagType> {
+                    var validTags = new Dictionary<string, CAATagType>(StringComparer.OrdinalIgnoreCase) {
                             { "issue", CAATagType.Issue },
                             { "issuewild", CAATagType.IssueWildcard },
                             { "iodef", CAATagType.Iodef },
