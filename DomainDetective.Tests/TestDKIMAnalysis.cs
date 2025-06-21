28fa457af6101c7826eb76a8517d6887b43c1a97
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-21 10:38:51 +0200

    fix: update assertions in TestDKIMAnalysis.cs for consistency and clarity

--- DomainDetective.Tests/TestDKIMAnalysis.cs
+++ DomainDetective.Tests/TestDKIMAnalysis.cs
@@ -1,4 +1,4 @@
-﻿namespace DomainDetective.Tests {
+namespace DomainDetective.Tests {
     public class TestDkimAnalysis {
         [Fact]
         public async Task TestDKIMRecord() {
@@ -7,14 +7,14 @@
             healthCheck.Verbose = true;
             await healthCheck.CheckDKIM(dkimRecord);
             foreach (var selector in healthCheck.DKIMAnalysis.AnalysisResults.Keys) {
-                Assert.True(selector == "default");
-                Assert.True(healthCheck.DKIMAnalysis.AnalysisResults[selector].Name == null);
-                Assert.True(healthCheck.DKIMAnalysis.AnalysisResults[selector].DkimRecord == dkimRecord);
+                Assert.Equal("default", selector);
+                Assert.Null(healthCheck.DKIMAnalysis.AnalysisResults[selector].Name);
+                Assert.Equal(dkimRecord, healthCheck.DKIMAnalysis.AnalysisResults[selector].DkimRecord);
                 Assert.True(healthCheck.DKIMAnalysis.AnalysisResults[selector].DkimRecordExists);
-                Assert.True(healthCheck.DKIMAnalysis.AnalysisResults[selector].Flags == null);
-                Assert.True(healthCheck.DKIMAnalysis.AnalysisResults[selector].HashAlgorithm == null);
-                Assert.True(healthCheck.DKIMAnalysis.AnalysisResults[selector].KeyType == "rsa");
-                Assert.True(healthCheck.DKIMAnalysis.AnalysisResults[selector].PublicKey == "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCqrIpQkyykYEQbNzvHfgGsiYfoyX3b3Z6CPMHa5aNn/Bd8skLaqwK9vj2fHn70DA+X67L/pV2U5VYDzb5AUfQeD6NPDwZ7zLRc0XtX+5jyHWhHueSQT8uo6acMA+9JrVHdRfvtlQo8Oag8SLIkhaUea3xqZpijkQR/qHmo3GIfnQIDAQAB");
+                Assert.Null(healthCheck.DKIMAnalysis.AnalysisResults[selector].Flags);
+                Assert.Null(healthCheck.DKIMAnalysis.AnalysisResults[selector].HashAlgorithm);
+                Assert.Equal("rsa", healthCheck.DKIMAnalysis.AnalysisResults[selector].KeyType);
+                Assert.Equal("MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCqrIpQkyykYEQbNzvHfgGsiYfoyX3b3Z6CPMHa5aNn/Bd8skLaqwK9vj2fHn70DA+X67L/pV2U5VYDzb5AUfQeD6NPDwZ7zLRc0XtX+5jyHWhHueSQT8uo6acMA+9JrVHdRfvtlQo8Oag8SLIkhaUea3xqZpijkQR/qHmo3GIfnQIDAQAB", healthCheck.DKIMAnalysis.AnalysisResults[selector].PublicKey);
                 Assert.True(healthCheck.DKIMAnalysis.AnalysisResults[selector].PublicKeyExists);
                 Assert.True(healthCheck.DKIMAnalysis.AnalysisResults[selector].StartsCorrectly);
                 Assert.True(healthCheck.DKIMAnalysis.AnalysisResults[selector].KeyTypeExists);
@@ -28,14 +28,14 @@
             };
             await healthCheck.Verify("evotec.pl", new[] { HealthCheckType.DKIM }, new[] { "selector1", "selector2" });
 
-            Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector1"].Name == "selector1-evotec-pl._domainkey.evotecpoland.onmicrosoft.com");
-            Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector1"].DkimRecord == "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCqrIpQkyykYEQbNzvHfgGsiYfoyX3b3Z6CPMHa5aNn/Bd8skLaqwK9vj2fHn70DA+X67L/pV2U5VYDzb5AUfQeD6NPDwZ7zLRc0XtX+5jyHWhHueSQT8uo6acMA+9JrVHdRfvtlQo8Oag8SLIkhaUea3xqZpijkQR/qHmo3GIfnQIDAQAB;");
-            Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector1"].DkimRecordExists == true);
-            Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector1"].Flags == null);
-            Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector1"].HashAlgorithm == null);
-            Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector1"].KeyType == "rsa");
-            Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector1"].StartsCorrectly == true);
-            Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector1"].PublicKey == "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCqrIpQkyykYEQbNzvHfgGsiYfoyX3b3Z6CPMHa5aNn/Bd8skLaqwK9vj2fHn70DA+X67L/pV2U5VYDzb5AUfQeD6NPDwZ7zLRc0XtX+5jyHWhHueSQT8uo6acMA+9JrVHdRfvtlQo8Oag8SLIkhaUea3xqZpijkQR/qHmo3GIfnQIDAQAB");
+            Assert.Equal("selector1-evotec-pl._domainkey.evotecpoland.onmicrosoft.com", healthCheck.DKIMAnalysis.AnalysisResults["selector1"].Name);
+            Assert.Equal("v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCqrIpQkyykYEQbNzvHfgGsiYfoyX3b3Z6CPMHa5aNn/Bd8skLaqwK9vj2fHn70DA+X67L/pV2U5VYDzb5AUfQeD6NPDwZ7zLRc0XtX+5jyHWhHueSQT8uo6acMA+9JrVHdRfvtlQo8Oag8SLIkhaUea3xqZpijkQR/qHmo3GIfnQIDAQAB;", healthCheck.DKIMAnalysis.AnalysisResults["selector1"].DkimRecord);
+            Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector1"].DkimRecordExists);
+            Assert.Null(healthCheck.DKIMAnalysis.AnalysisResults["selector1"].Flags);
+            Assert.Null(healthCheck.DKIMAnalysis.AnalysisResults["selector1"].HashAlgorithm);
+            Assert.Equal("rsa", healthCheck.DKIMAnalysis.AnalysisResults["selector1"].KeyType);
+            Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector1"].StartsCorrectly);
+            Assert.Equal("MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCqrIpQkyykYEQbNzvHfgGsiYfoyX3b3Z6CPMHa5aNn/Bd8skLaqwK9vj2fHn70DA+X67L/pV2U5VYDzb5AUfQeD6NPDwZ7zLRc0XtX+5jyHWhHueSQT8uo6acMA+9JrVHdRfvtlQo8Oag8SLIkhaUea3xqZpijkQR/qHmo3GIfnQIDAQAB", healthCheck.DKIMAnalysis.AnalysisResults["selector1"].PublicKey);
             Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector1"].PublicKeyExists);
 
             Assert.True(healthCheck.DKIMAnalysis.AnalysisResults["selector2"].Name == "selector2-evotec-pl._domainkey.evotecpoland.onmicrosoft.com");
