a364d5a4ce8adab966544a23177ede42cbde866f
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-23 10:45:26 +0200

    Add XML docs for DNSSEC analysis

--- DomainDetective.Tests/TestDNSSECAnalysis.cs
+++ DomainDetective.Tests/TestDNSSECAnalysis.cs
@@ -7,7 +7,9 @@ namespace DomainDetective.Tests {
 
             Assert.NotEmpty(healthCheck.DNSSecAnalysis.DnsKeys);
             Assert.True(healthCheck.DNSSecAnalysis.AuthenticData);
+            Assert.True(healthCheck.DNSSecAnalysis.DsAuthenticData);
             Assert.True(healthCheck.DNSSecAnalysis.DsMatch);
+            Assert.True(healthCheck.DNSSecAnalysis.ChainValid);
         }
     }
 }
