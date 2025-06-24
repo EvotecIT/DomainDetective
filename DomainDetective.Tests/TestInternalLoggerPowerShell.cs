4adae0fea4b96fdf35c607ef9e82a7668f619da4
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-24 09:21:13 +0200

    feat(ps): ✨ increment error record ids

---
+++ DomainDetective.Tests/TestInternalLoggerPowerShell.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Management.Automation;
+using DomainDetective;
+using DomainDetective.PowerShell;
+
+namespace DomainDetective.Tests {
+    public class TestInternalLoggerPowerShell {
+        [Fact]
+        public void ErrorRecordIdIncrements() {
+            var logger = new InternalLogger();
+            var records = new List<ErrorRecord>();
+            var psLogger = new InternalLoggerPowerShell(logger, null, null, null, records.Add);
+
+            logger.WriteError("first");
+            logger.WriteError("second");
+
+            Assert.Equal(2, records.Count);
+            Assert.Equal("1", records[0].FullyQualifiedErrorId);
+            Assert.Equal("2", records[1].FullyQualifiedErrorId);
+        }
+    }
+}
