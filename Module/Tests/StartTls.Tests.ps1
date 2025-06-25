0bfa3f64ebdf295866ae257ea86ce06ed0af8b6f
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-25 07:23:01 +0200

    test(powershell): ✅ update StartTls tests

--- Module/Tests/StartTls.Tests.ps1
+++ Module/Tests/StartTls.Tests.ps1
@@ -1,13 +1,9 @@
 Describe 'Test-StartTls cmdlet' {
-    It 'executes and returns analysis on default port' {
+    It 'exposes Port parameter' {
         Import-Module "$PSScriptRoot/../DomainDetective.psd1" -Force
-        $result = Test-StartTls -DomainName 'example.com' -DnsEndpoint CloudflareWireFormat
-        $result | Should -Not -BeNullOrEmpty
-    }
-    It 'accepts custom port parameter' {
-        Import-Module "$PSScriptRoot/../DomainDetective.psd1" -Force
-        $result = Test-StartTls -DomainName 'example.com' -DnsEndpoint CloudflareWireFormat -Port 587
-        $result | Should -Not -BeNullOrEmpty
+        $command = Get-Command Test-StartTls
+        $command.Parameters.Keys | Should -Contain 'Port'
+        [DomainDetective.PowerShell.CmdletTestStartTls]::new().Port | Should -Be 25
     }
     It 'throws if DomainName is empty' {
         Import-Module "$PSScriptRoot/../DomainDetective.psd1" -Force
