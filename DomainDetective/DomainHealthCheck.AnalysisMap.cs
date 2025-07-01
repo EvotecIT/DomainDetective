e16c748d8ebeccf06353ddb53c0edd63eb635508
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-07-01 08:59:40 +0200

    style(core): 💄 enforce block-scoped namespace

--- DomainDetective/DomainHealthCheck.AnalysisMap.cs
+++ DomainDetective/DomainHealthCheck.AnalysisMap.cs
@@ -1,9 +1,23 @@
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 
-namespace DomainDetective;
+namespace DomainDetective
+{
+    public partial class DomainHealthCheck {
+        private static readonly IReadOnlyDictionary<HealthCheckType, PropertyInfo?> AnalysisPropertyMap;
 
-public partial class DomainHealthCheck {
+    static DomainHealthCheck()
+    {
+        var map = new Dictionary<HealthCheckType, PropertyInfo?>();
+        foreach (HealthCheckType check in Enum.GetValues(typeof(HealthCheckType)))
+        {
+            var property = typeof(DomainHealthCheck).GetProperty($"{check}Analysis");
+            map[check] = property;
+        }
+
+        AnalysisPropertyMap = map;
+    }
     /// <summary>
     ///     Creates a dictionary mapping each <see cref="HealthCheckType"/> to
     ///     the corresponding analysis result instance.
@@ -13,15 +27,14 @@ public partial class DomainHealthCheck {
     /// </returns>
     public IReadOnlyDictionary<HealthCheckType, object?> GetAnalysisMap()
     {
-        var map = new Dictionary<HealthCheckType, object?>(
-            Enum.GetValues(typeof(HealthCheckType)).Length);
+        var map = new Dictionary<HealthCheckType, object?>(AnalysisPropertyMap.Count);
 
-        foreach (HealthCheckType check in Enum.GetValues(typeof(HealthCheckType)))
+        foreach (var kvp in AnalysisPropertyMap)
         {
-            var property = GetType().GetProperty($"{check}Analysis");
-            map[check] = property?.GetValue(this);
+            map[kvp.Key] = kvp.Value?.GetValue(this);
         }
 
         return map;
     }
+}
 }
