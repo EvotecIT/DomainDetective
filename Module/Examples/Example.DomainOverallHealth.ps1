12626dc5bfd9a12736d57f1d2bc37ebcd7c888f7
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-09-03 15:15:15 +0200

    feat(reports): ✨ update report generation to include export path
    * Added `-ExportPath` parameter to `Test-DDDomainOverallHealth` for better report management.
    * Removed obsolete HTML report file to streamline the reporting process.

--- Module/Examples/Example.DomainOverallHealth.ps1
+++ Module/Examples/Example.DomainOverallHealth.ps1
@@ -1,4 +1,4 @@
 ﻿Import-Module $PSScriptRoot\..\DomainDetective.psd1 -Force
 
-$DomainOverallHealth = Test-DDDomainOverallHealth -DomainName 'evotec.pl' -Verbose -ExportFormat Html
+$DomainOverallHealth = Test-DDDomainOverallHealth -DomainName 'evotec.pl' -Verbose -ExportFormat Html -ExportPath "$PSScriptRoot\Reports"
 $DomainOverallHealth | Format-List
